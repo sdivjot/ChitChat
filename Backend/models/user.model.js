a36e7258a485a94f612e5bd985e856351a959c43
sdivjot <imt_2021031@iiitm.ac.in>
2024-06-26 20:13:20 +0530

    deployment

---
+++ Backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    imagePath: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPassword: {
+        type: String,
+    },
+}, { timestamps: true }); // createdAt & updatedAt
+
+const User = mongoose.model("User", userSchema);
+export default User;
